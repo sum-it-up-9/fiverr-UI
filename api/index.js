1e005eeaef45a095d37ab46f9dc84666062f5e93
sum-it-up-9 <66559261+sum-it-up-9@users.noreply.github.com>
2024-03-04 16:20:02 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -49,13 +49,13 @@ Connection(URL);
 
 
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/gigs", gigRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/conversations", conversationRoute);
-app.use("/api/messages", messageRoute);
-app.use("/api/reviews", reviewRoute);
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/gigs", gigRoute);
+app.use("/orders", orderRoute);
+app.use("/conversations", conversationRoute);
+app.use("/messages", messageRoute);
+app.use("/reviews", reviewRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
