cd9a1dcf6a72a35655e58a6c54ffc5181a6d50ca
sum-it-up-9 <66559261+sum-it-up-9@users.noreply.github.com>
2024-03-04 15:48:04 +0530

    Update newRequest.js

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://fiverr-frontend-nine.vercel.app/api",
   withCredentials: true,
 });
 
